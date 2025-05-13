d936b3d26c2bce39622a73e8d813d7d84b889a9e
Bushra-Firdose <bushrafirdose.parwam@gmail.com>
2025-05-13 14:38:51 +0530

    first commit

--- EclipseWorkplace.java/springSecurityDemo5/src/main/java/com/springSecurityDemo/springSecurityDemo/Controller/HomeController.java
+++ EclipseWorkplace.java/springSecurityDemo5/src/main/java/com/springSecurityDemo/springSecurityDemo/Controller/HomeController.java
@@ -1,46 +1,83 @@
 package com.springSecurityDemo.springSecurityDemo.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.Model; 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.springSecurityDemo.springSecurityDemo.DTO.StudentDTO;
-import com.springSecurityDemo.springSecurityDemo.model.Student;
 import com.springSecurityDemo.springSecurityDemo.service.StudentService;
 
+
+//we have anotation for Rest API is @RestController 
 @Controller
 public class HomeController {
 	
+	@Autowired
 	private StudentService studentService;
 	
-	@GetMapping({"/",""})
-	public String home() {
-		return "home";
-	}
 	
-	@GetMapping("/about")
-	public String about() {
-		return "about";
-	}
 	
-	@GetMapping("/contact")
-	public String contact() {
-		return "contact";
+	 public HomeController(StudentService studentService) {
+		super();
+		this.studentService = studentService;
 	}
-	@GetMapping("/register")
-	public String register(Model model) {
-		StudentDTO studentDTO =new StudentDTO();
-		model.addAttribute("studentDTO" ,studentDTO);
-		return "register";
-	}
-	@PostMapping("/register")
-	public String register(@ModelAttribute StudentDTO studentDTO, RedirectAttributes redirectAttributes) {
-		studentService.storeStudent(studentDTO);
-		redirectAttributes.addFlashAttribute("Success", "Student saved successfully");
-		return "redirect:/";
-	}
+
+
+	@GetMapping({"/"," "})
+	 public String home() {
+		 return "home";
+	 }
+	 
+	 
+	 @GetMapping("/about")
+	 public String about() {
+		 return "about";
+	 }
+	 
+	 @GetMapping("/contact")
+	 public String contact() {
+		 return "contact";
+	 }
+	 
+	 @GetMapping("/register")
+	 public String register(Model model) {
+		 StudentDTO studentDTO = new StudentDTO();
+		 model.addAttribute("studentDTO",studentDTO); 
+		 return "register";
+	 }
+	 
+	 @PostMapping("/register")
+	 public String register(@ModelAttribute StudentDTO studentDTO, RedirectAttributes redirectAttributes) {
+		 studentService.storeStudent(studentDTO);
+		 redirectAttributes.addFlashAttribute("successMessage","Student saved succesfully");
+		 return "redirect:/";
+	 } 
+	 
+	 @GetMapping("/std_details")
+	 public String std_details() {
+		 return "std_details";
+	 }
+	 
+	 
+	 @GetMapping("/login")
+	 public String login() {
+		 return "login";
+	 }
+	 @GetMapping("/user")
+		public String user() {
+			return "user";
+		}
+
+		@GetMapping("/admin")
+		public String admin() {
+			return "admin";
+		}
+
+	 
+	 
 
 }
--- EclipseWorkplace.java/springSecurityDemo5/src/main/java/com/springSecurityDemo/springSecurityDemo/config/Securityconfig.java
+++ EclipseWorkplace.java/springSecurityDemo5/src/main/java/com/springSecurityDemo/springSecurityDemo/config/Securityconfig.java
@@ -3,12 +3,15 @@ package com.springSecurityDemo.springSecurityDemo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.springSecurityDemo.springSecurityDemo.repository.StudentRepo;
 import com.springSecurityDemo.springSecurityDemo.service.CustomUserDetailsService;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class Securityconfig {
@@ -35,6 +38,21 @@ public class Securityconfig {
 		provider.setPasswordEncoder(passwordEncoder());
 		provider.setUserDetailsService(userDetailsService());
 		return provider;
+	}
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+		return http
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/","/about" ,"/contact" ,"/register").permitAll()
+						.anyRequest().authenticated()
+						)
+				.formLogin(login -> login
+						.loginPage("/login")
+						.loginProcessingUrl("/login")
+						.defaultSuccessUrl("/std-edtails",true)
+						.permitAll()
+						)
+				.build();
 	}
 	
 	
